4d95e8514c631665e780ad24acfc63a6c84bce7d
Francisco leon <22012000fdlq@gmail.com>
2021-10-15 11:24:36 -0300

    feo pero funciona :c

---
+++ src/components/Card.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <el-row>
+      <el-col
+        :sm="12" :md="8" :lg="6"
+        v-for="(personaje, index) in personajes"
+        :key="index"
+        :offset="index > 0 ? 2 : 0"
+        justify="center"
+        type="flex"
+      >
+        <el-card :body-style="{ padding: '0px' }">
+          <img :src="personaje.imagen" class="image" />
+          <div style="padding: 14px;">
+            <el-text>{{ personaje.nombre }}</el-text>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    personajes: [
+      {
+        id: 1,
+        nombre: "Morty",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      },
+      {
+        id: 2,
+        nombre: "Rick",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      },
+      {
+        id: 3,
+        nombre: "Summer",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+      },
+      {
+        id: 4,
+        nombre: "Beth",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+      },
+      {
+        id: 5,
+        nombre: "Jerry",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+.el-col {
+  padding: 20px;
+}
+
+
+</style>
